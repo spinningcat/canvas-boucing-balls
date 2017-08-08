3302dfe295064f4f5cff6e9bfd402e3995a0bd5a
spinningcat <spinningcat@debian>
2017-08-08 16:05:54 +0300

    adding collision

--- script/bouncing.js
+++ script/bouncing.js
@@ -6,8 +6,8 @@ var height = canvas.height = window.innerHeight;
 
 // function to generate random number
 
-function random(min,max) {
-  var num = Math.floor(Math.random()*(max-min)) + min;
+function random(min, max) {
+  var num = Math.floor(Math.random() * (max - min)) + min;
   return num;
 }
 
@@ -18,8 +18,8 @@ horizontal and vertical velocity (velX and velY) — each ball is given a horizo
 color — each ball gets a color.
 size — each ball gets a size — this will be its radius, in pixels.*/
 
-function Ball(x,y,velX,velY,color,size){
-	this.x = x;
+function Ball(x, y, velX, velY, color, size) {
+  this.x = x;
   this.y = y;
   this.velX = velX;
   this.velY = velY;
@@ -29,53 +29,69 @@ function Ball(x,y,velX,velY,color,size){
 
 // drawing the balls
 
-Ball.prototype.draw = function()
-{
-		ctx.beginPath();
-		ctx.fillStyle = this.color;
-    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-    ctx.fill();
+Ball.prototype.draw = function() {
+  ctx.beginPath();
+  ctx.fillStyle = this.color;
+  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+  ctx.fill();
 }
 
 //var testBall = new Ball(50, 100, 4, 4, 'blue', 10);
 //testBall.draw();
 
-Ball.prototype.update = function(){
-  if ((this.x + this.size) >= width) {
-    this.velX = -(this.velX);
-  }
+Ball.prototype.update = function() {
+    if ((this.x + this.size) >= width) {
+      this.velX = -(this.velX);
+    }
 
-  if ((this.x - this.size) <= 0) {
-    this.velX = -(this.velX);
-  }
+    if ((this.x - this.size) <= 0) {
+      this.velX = -(this.velX);
+    }
 
-  if ((this.y + this.size) >= height) {
-    this.velY = -(this.velY);
-  }
+    if ((this.y + this.size) >= height) {
+      this.velY = -(this.velY);
+    }
 
-  if ((this.y - this.size) <= 0) {
-    this.velY = -(this.velY);
-  }
+    if ((this.y - this.size) <= 0) {
+      this.velY = -(this.velY);
+    }
 
-  this.x += this.velX;
-  this.y += this.velY;
-}
-//animating the ball
+    this.x += this.velX;
+    this.y += this.velY;
+  }
+  //animating the ball
 
 var balls = [];
 
+
+// lets add 2d collision
+
+Ball.prototype.collisionDetect = function() {
+  for (var j = 0; j < balls.length; j++) {
+    if (!(this === balls[j])) {
+      var dx = this.x - balls[j].x;
+      var dy = this.y - balls[j].y;
+      var distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance < this.size + balls[j].size) {
+        balls[j].color = this.color = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')';
+      }
+    }
+
+  }
+}
 function loop() {
   ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
   ctx.fillRect(0, 0, width, height);
 
   while (balls.length < 25) {
     var ball = new Ball(
-      random(0,width),
-      random(0,height),
-      random(-7,7),
-      random(-7,7),
-      'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) +')',
-      random(10,20)
+      random(0, width),
+      random(0, height),
+      random(-7, 7),
+      random(-7, 7),
+      'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')',
+      random(10, 20)
     );
     balls.push(ball);
   }
@@ -83,9 +99,9 @@ function loop() {
   for (var i = 0; i < balls.length; i++) {
     balls[i].draw();
     balls[i].update();
+    balls[i].collisionDetect();
   }
 
   requestAnimationFrame(loop);
 }
 loop();
-
